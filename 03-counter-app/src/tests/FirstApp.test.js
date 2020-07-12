cad39e415616236e0d3b44387879fc42f2cf5fe6
chrisloarryn <chrisloarryn@gmail.com>
2020-07-11 22:05:22 -0400

    tests w/enzyme

--- 03-counter-app/src/tests/FirstApp.test.js
+++ 03-counter-app/src/tests/FirstApp.test.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { shallow } from 'enzyme'
+
 import FirstApp from '../FirstApp'
 
+
+/*  
+  //   test('<FirstApp /> should render "Hello, I am Goku" message" ', () => {
+  //     const greeting = 'Hello, I am Goku'
+  //     const { getByText } = render(<FirstApp message={greeting} />)
+  //     expect(getByText(greeting)).toBeInTheDocument()
+  //   })
+*/
 describe('Test in <FirstApp />', () => {
-  test('<FirstApp /> should render "Hello, I am Goku" message" ', () => {
-    const greeting = 'Hello, I am Goku'
-    const { getByText } = render(<FirstApp message={greeting} />)
-    expect(getByText(greeting)).toBeInTheDocument()
+  test('<FirstApp /> should be rendered correctly', () => {
+    const message = 'Hello, I am Goku'
+    const wrapper = shallow(<FirstApp message={message} />)
+    
+    expect(wrapper).toMatchSnapshot()
+ 
   })
 })

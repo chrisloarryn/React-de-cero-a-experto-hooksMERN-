3a47a19725119b771bf583836cc029f05dc266b2
chrisloarryn <chrisloarryn@gmail.com>
2020-08-22 21:26:34 -0400

    Update useFetchGifs.test.js

--- 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
+++ 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
@@ -7,13 +7,27 @@ describe(`Test in hook useFetchGifs`, () => {
 
     const category = 'One Piece'
 
-    test(`should return initial state`, () => {
+    test(`should return initial state`, async () => {
 
-        const { result } = renderHook(() => useFetchGifs(category))
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGifs(category))
         const { data:images, loading } = result.current
         
+        await waitForNextUpdate()
+
         expect(images).toEqual([])
         expect(loading).toBe(true)
+    })
+
+    test(`should return an Array of imgs and 'loading' in false`, async () => {
+
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGifs(category))
+        await waitForNextUpdate()
+
+        const { data:images, loading } = result.current
+
+        expect(images.length).toBe(10)
+        expect(loading).toBe(false)
+
     })
 
 })

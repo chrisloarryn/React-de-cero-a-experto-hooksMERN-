a5d936e25ca217eccb78004e4175d0c22c72f831
Cristóbal Contreras <44170429+chrisloarryn@users.noreply.github.com>
2020-09-22 02:51:48 -0300

    Custom hook, useForm

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 // import { CounterApp } from "./components/01-useState/CounterApp";
 // import { HookApp } from "./HookApp";
 // import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
-import { SimpleForm } from "./components/02-useEffect/SimpleForm";
 
-ReactDOM.render(<SimpleForm />, document.getElementById("root"));
+// import { SimpleForm } from "./components/02-useEffect/SimpleForm";
+import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook'
+
+ReactDOM.render(<FormWithCustomHook />, document.getElementById("root"));
